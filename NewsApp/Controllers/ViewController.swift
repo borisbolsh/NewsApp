c0f32b52da3ba995c624ac384b58a15c3490ddfc
borisbolsh <bolsh.bo@yandex.ru>
2021-12-09 23:43:35 +0300

    initial commit

---
+++ NewsApp/Controllers/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  NewsApp
+//
+//  Created by Boris Bolshakov on 9.12.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        view.backgroundColor = .link
+    }
+
+
+}
+
---
+++ NewsApp/Default/SceneDelegate.swift
@@ -0,0 +1,28 @@
+//
+//  SceneDelegate.swift
+//  NewsApp
+//
+//  Created by Boris Bolshakov on 9.12.21.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+    
+    var window: UIWindow?
+    
+    
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        
+        
+        window = UIWindow(windowScene: windowScene)
+        window?.rootViewController = UINavigationController(rootViewController: ViewController())
+        window?.makeKeyAndVisible()
+    }
+    
+    
+    
+}
+
